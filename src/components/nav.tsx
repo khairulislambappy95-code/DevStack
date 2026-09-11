84e8cc1b62a1f351bc04a10d2dcfbf6cbde53740
khairulislambappy95-code <khairulislambappy95@gmail.com>
2026-09-11 20:53:42 +0600

    Nav uppdated

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -4,15 +4,15 @@ export function Nav() {
     <nav className="flex items-center justify-between px-8 py-4 bg-white shadow-md">
       <img src={logo} className="logo"/>
       <ul className="flex space-x-4 text-gray-700">
-        <li>Home</li>
+        <li className="text-pink-500 font-bold">Home</li>
         <li>Techologies</li>
         <li>Projects</li>
         <li>About</li>
         <li>Contact</li>  
       </ul>
       <div className="flex space-x-4">
-      <button className="btn btn-sign-in">Sign In</button>
-      <button className="btn btn-secondary rounded-md">Sign Up</button>
+      <button className="btn btn-ghost">Sign In</button>
+      <button className="btn btn-secondary">Sign Up</button>
       </div>
     </nav>
   )
