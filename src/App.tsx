c44a6518a2c8bca03affda68b6443bdaa6f6a0f0
khairulislambappy95-code <khairulislambappy95@gmail.com>
2026-09-11 20:45:30 +0600

    logo froblem fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
+import { Nav } from './components/nav'
 import './index.css'
-import logo from './assets/logo-text.png'
+
 
 function App() {
 
   return (
     <>
-    <nav className="flex items-center justify-between px-8 py-4 bg-white shadow-md">
-      <img src={logo} className="logo"/>
-      <ul className="flex space-x-4 text-gray-700">
-        <li>Home</li>
-        <li>Techologies</li>
-        <li>Projects</li>
-        <li>About</li>
-        <li>Contact</li>  
-      </ul>
-      <div className="flex space-x-4">
-      <button className="btn btn-sign-in">Sign In</button>
-      <button className="btn btn-sign-up">Sign Up</button>
-      </div>
-    </nav>
+      <Nav />
     </>
   )
 }
---
+++ src/components/nav.tsx
@@ -0,0 +1,19 @@
+import logo from '../assets/logo-text.png'
+export function Nav() {
+  return (
+    <nav className="flex items-center justify-between px-8 py-4 bg-white shadow-md">
+      <img src={logo} className="logo"/>
+      <ul className="flex space-x-4 text-gray-700">
+        <li>Home</li>
+        <li>Techologies</li>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>  
+      </ul>
+      <div className="flex space-x-4">
+      <button className="btn btn-sign-in">Sign In</button>
+      <button className="btn btn-secondary rounded-md">Sign Up</button>
+      </div>
+    </nav>
+  )
+}
